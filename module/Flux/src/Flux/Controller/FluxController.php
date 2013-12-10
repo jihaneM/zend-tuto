1bf1e108206fb164bcd9eea6de06de1ed2ae690d
jihaneM <jihanemoustakbal@gmail.com>
2013-12-10 22:29:28 +0100

    ajout edit-delet

--- module/Flux/src/Flux/Controller/FluxController.php
+++ module/Flux/src/Flux/Controller/FluxController.php
@@ -1,49 +1,128 @@
-<?php
+<?php 
 namespace Flux\Controller;
-use Zend\Mvc\Controller\AbstractActionController;
-use Zend\View\Model\ViewModel;
 
+ use Zend\Mvc\Controller\AbstractActionController;
+ use Zend\View\Model\ViewModel;
+ use Flux\Model\Flux;
+ use Flux\Model\FluxTable;
+ use Flux\Form\FluxForm;
+ 
  class FluxController extends AbstractActionController
  {
- 	
- 	protected $fluxTable;
- 	
-	 	public function getAlbumTable()
-	 	{
-	 	
-	 		if (!$this->fluxTable) {
-	 			$sm = $this->getServiceLocator();
-	 			$this->fluxTable = $sm->get('Flux\Model\FluxTable');
-	 		}
-	 		return $this->fluxTable;
-	 	}
-	 	
-	 	public function indexAction()
-	 	{
-	 		return new ViewModel(array(
-	 				'fluxV' => $this->getAlbumTable()->fetchAll(),
-	 		));
-	 	}
- 	 
- 	
+     protected $fluxTable;
      
+     public function indexAction()
+     {
+         return new ViewModel(array(
+         		'fluxV' => $this->getFluxTable()->fetchAll(),
+         ));
+     }
 
      public function addAction()
      {
+         $form = new FluxForm();
+         $form->get('submit')->setValue('Add');
+         
+         $request = $this->getRequest();
+         if ($request->isPost()) {
+         	$flux = new Flux();
+         	$form->setInputFilter($flux->getInputFilter());
+         	$form->setData($request->getPost());
+         
+         	if ($form->isValid()) {
+         		$album->exchangeArray($form->getData());
+         		$this->getFluxTable()->saveFlux($flux);
+         
+         		// Redirect to list of albums
+         		return $this->redirect()->toRoute('flux');
+         	}
+         }
+         return array('form' => $form);
      }
 
+     
+     
      public function editAction()
      {
+         $id = (int) $this->params()->fromRoute('id', 0);
+         if (!$id) {
+         	return $this->redirect()->toRoute('flux', array(
+         			'action' => 'add'
+         	));
+         }
+         
+         // Get the Album with the specified id.  An exception is thrown
+         // if it cannot be found, in which case go to the index page.
+         try {
+         	$flux = $this->getFluxTable()->getFlux($id);
+         }
+         catch (\Exception $ex) {
+         	return $this->redirect()->toRoute('flux', array(
+         			'action' => 'index'
+         	));
+         }
+         
+         $form  = new FluxForm();
+         $form->bind($flux);
+         $form->get('submit')->setAttribute('value', 'Edit');
+         
+         $request = $this->getRequest();
+         if ($request->isPost()) {
+         	$form->setInputFilter($flux->getInputFilter());
+         	$form->setData($request->getPost());
+         
+         	if ($form->isValid()) {
+         		$this->getFluxTable()->saveFlux($flux);
+         
+         		// Redirect to list of albums
+         		return $this->redirect()->toRoute('flux');
+         	}
+         }
+         
+         return array(
+         		'id' => $id,
+         		'form' => $form,
+         );
      }
 
      public function deleteAction()
      {
+         $id = (int) $this->params()->fromRoute('id', 0);
+         if (!$id) {
+         	return $this->redirect()->toRoute('flux');
+         }
+         
+         $request = $this->getRequest();
+         if ($request->isPost()) {
+         	$del = $request->getPost('del', 'No');
+         
+         	if ($del == 'Yes') {
+         		
+         		$id = (int) $request->getPost('id');
+         		
+         		$this->getFluxTable()->deleteFlux($id);
+         	}
+         
+         	// Redirect to list of albums
+         	return $this->redirect()->toRoute('flux');
+         }
+         
+         return array(
+         		'id'    => $id,
+         		'flux' => $this->getFluxTable()->getFlux($id)
+         );
      }
      
-  
      
-     
-     
-     
-     
+     public function getFluxTable()
+     {
+         
+     	if (!$this->fluxTable) {
+     		$sm = $this->getServiceLocator();
+     		$this->fluxTable = $sm->get('Flux\Model\FluxTable');
+     	}
+     	return $this->fluxTable;
+     }
  }
+ 
+ ?>
--- module/Flux/src/Flux/Model/Flux.php
+++ module/Flux/src/Flux/Model/Flux.php
@@ -1,16 +1,97 @@
-<?php
+<?php 
 namespace Flux\Model;
 
-class Flux
-{
-	public $id;
-	public $artist;
-	public $title;
+use Zend\InputFilter\InputFilter;
+use Zend\InputFilter\InputFilterAwareInterface;
+use Zend\InputFilter\InputFilterInterface;
 
-	public function exchangeArray($data)
-	{
-		$this->id     = (!empty($data['id'])) ? $data['id'] : null;
-		$this->artist = (!empty($data['artist'])) ? $data['artist'] : null;
-		$this->title  = (!empty($data['title'])) ? $data['title'] : null;
-	}
-}
+ class Flux implements InputFilterAwareInterface
+ {
+     public $id;
+     public $artist;
+     public $title;
+
+     public function exchangeArray($data)
+     {
+         $this->id     = (!empty($data['id'])) ? $data['id'] : null;
+         $this->artist = (!empty($data['artist'])) ? $data['artist'] : null;
+         $this->title  = (!empty($data['title'])) ? $data['title'] : null;
+     }
+     
+     
+     
+     public function getArrayCopy()
+     {
+     	return get_object_vars($this);
+     }
+     
+     
+     
+     public function setInputFilter(InputFilterInterface $inputFilter)
+     {
+     	throw new \Exception("Not used");
+     }
+     
+     
+     
+     public function getInputFilter()
+     {
+     	
+     	if (!$this->p) {
+     		$p = new InputFilter();
+     
+     		$p->add(array(
+     				'name'     => 'id',
+     				'required' => true,
+     				'filters'  => array(
+     						array('name' => 'Int'),
+     				),
+     		));
+     
+     		$p->add(array(
+     				'name'     => 'artist',
+     				'required' => true,
+     				'filters'  => array(
+     						array('name' => 'StripTags'),
+     						array('name' => 'StringTrim'),
+     				),
+     				'validators' => array(
+     						array(
+     								'name'    => 'StringLength',
+     								'options' => array(
+     										'encoding' => 'UTF-8',
+     										'min'      => 1,
+     										'max'      => 100,
+     								),
+     						),
+     				),
+     		));
+     
+     		$p->add(array(
+     				'name'     => 'title',
+     				'required' => true,
+     				'filters'  => array(
+     						array('name' => 'StripTags'),
+     						array('name' => 'StringTrim'),
+     				),
+     				'validators' => array(
+     						array(
+     								'name'    => 'StringLength',
+     								'options' => array(
+     										'encoding' => 'UTF-8',
+     										'min'      => 1,
+     										'max'      => 100,
+     								),
+     						),
+     				),
+     		));
+     
+     		$this->p = $p;
+     	}
+     
+     	return $this->p;
+     }
+    
+ }
+ 
+ ?>
